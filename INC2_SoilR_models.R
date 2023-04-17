15854d8aad26ecb89ecf843d18db22b152ad708b
michellesunwang <michelle.s.wang.th@dartmouth.edu>
2023-04-17 09:25:52 -0400

    To be reviewed by Caitlin
    Models are not aligning with data when k1 and gamma are fixed. Fixing k1 and gamma with INC1 data seems to work fine. But when I let the program guess all parameters, models seem to fit for INC2.

--- INC2_SoilR_models.R
+++ INC2_SoilR_models.R
@@ -7,11 +7,26 @@ library(SoilR)
 library(FME)
 
 # Intializations
-i = 1 # CHANGE IF INTERESTED, treatment type 
+i = 2 # CHANGE IF INTERESTED, treatment type 
+#Sample key as follows:
+# C/O means closed/open valve
+#'DASE_C' = 1 standard dosage
+#'DASE_O' = 2 standard dosage
+#'AD_S' = 3 standard dosage
+#'POET_S' = 4 standard dosage
+#'NREL_S' = 5 standard dosage
+#'AD_N' = 6 new ie. halved dosage
+#'POET_N' = 7 new dosage
+#'NREL_N' = 8 new dosage 
+#'CS_N' = 9 new dosage
+#'GWC16' = 10 PALOUSE SOIL CONTROL 1
+#'GWC20' = 11 PALOUSE SOIL CONTROL 2
 n = 2 # start this at 2 for things to save correctly, column index, save to right column in fitFrames
 
 # Initial Soil + Residue input into treatment jars [mg C]
-Cinits <- 1000*c(1.199218125, 1.198987771, 1.202890795, 1.208769544, 1.280327308, 0.869183259, 0.87199067, 0.907076619, 0.880866037, 0.540873971, 0.535095807) # these numbers reflect if I average C per treatment, Information from INC3 -> CombinedIRMS -> Treatment_Calculations
+#Cinits <- 1000*c(1.199218125, 1.198987771, 1.202890795, 1.208769544, 1.280327308, 0.869183259, 0.87199067, 0.907076619, 0.880866037, 0.540873971, 0.535095807) # these numbers reflect if I average C per treatment, Information from INC3 -> CombinedIRMS -> Treatment_Calculations
+#Cinits <- c(514.4336596, 1145.656643, 1059.347782, 1151.299211, 1188.126723) # these numbers reflect if I average C per treatment, Information from INC2 -> IRMS -> "IRMS_summary" -> IRMS_Pre
+Cinits <- c(1199.218125, 1198.987771, 1202.890795, 1208.769544, 1280.327308, 869.183259, 871.99067, 907.076619, 880.866037, 540.873971, 535.095807) # these numbers reflect if I average C per treatment, Information from INC3 -> CombinedIRMS -> Treatment_Calculations
 
 num_treatments = 11
 AICc_1p_tot <- numeric(length=num_treatments)
@@ -25,6 +40,8 @@ twopp_par <- list(length = num_treatments)
 twops_par <- list(length = num_treatments)
 
 k_soil = 0.000631349
+
+# gamma_soil <- c(1,	0.446666266,	0.490614741,	0.44600461,	0.437380269)
 
 gamma_soil <- c(0.446229175
                 ,0.44601221
@@ -40,38 +57,17 @@ gamma_soil <- c(0.446229175
 
 # TIME VECTOR
 #days <- 0:3650
-end_day <- 365*100  ### CHANGE TIME VECTOR [days] ### 
-# days <- seq(from = 1, to = end_day, by = 1)
-days <- seq(from = 1, to = end_day, by = 365/5)
+end_day <- 365*10  ### CHANGE TIME VECTOR [days] ### 
+days <- seq(from = 1, to = end_day, by = 1)
+# days <- seq(from = 1, to = end_day, by = 365/5)
 #days <- seq(0, round(last(CO2flux$time)))  # this days vector is just the length of your data
 
-# INPUT VECTOR - CHANGE HERE, Either you can do inputs_frame = 0 for no inputs, or use the other loop
-# C from residue for each treatment [mg], COMMENT OUT ONE SET
-
-# INPUTS 1
-# inputs_valsP <- c(0,	632.8138108,	544.071363,	637.547334,	672.8124268)	 # these numbers reflect if I average the residues in each treatment, Information from INC2 -> IRMS -> "IRMS_summary" -> IRMS_Pre  
-# inputs_valsV <- c(0,	634.9023052,	539.3092504,	635.4292365,	672.8124268)
-
-# INPUTS 2
-#inputs_vals <- c(CinitsP[1], CinitsP[2]-CinitsP[1], CinitsP[3]-CinitsP[1], CinitsP[4]-CinitsP[1], CinitsP[5]-CinitsP[1])	 # these numbers reflect if I subtract out soil from Cinits vectors
-
-# # OPTION 1: Inputs every end of year, 99 inputs in dataframe, this only works for inputs w/ time steps of 365/5 days
-# inputs_mainframe <- data.frame(days, matrix(0, length(days), 5))
-# colnames(inputs_mainframe) <- c('days', '1', '2', '3', '4', '5')
-# a = 2 # column counter
-# b = 1 # inputs_vals counter
-# while (a < 7) {
-#   inputs_mainframe[seq(from = 6, to = length(days), by = 5), a] <-  inputs_valsP[b] ### CHANGE V/P HERE
-#   a = a + 1
-#   b = b + 1
-# }
-# write.csv(inputs_mainframe, file = 'inputframeP2.csv') ### CHANGE V/P ###
-
-# OPTION 2: 0 inputs throughout year after first one
+## INPUTS
+# OPTION 1: 0 inputs throughout year after first one
 inputs_frame = 0
 
-# CUMM. CO2 PRODUCED VECTOR
-totalfitCumm <- as.data.frame(matrix(nrow = length(days), ncol = 11*3+1))
+# CUMM. CO2 PRODUCED VECTOR, INITIALIZE dataframe
+totalfitCumm <- as.data.frame(matrix(nrow = length(days), ncol = num_treatments*3+1))
 totalfitCumm[, 1] <- days 
 
 # Graphing theme
@@ -83,10 +79,10 @@ theme_C <- theme_light() +
         plot.title = element_text(hjust = 0.5))
 
 # Read in data
-CO2flux_0 <- read.csv("INC2data_mod.csv", header=TRUE) ### CHANGE V/P ###
+CO2flux_0 <- read.csv("INC2data_mod.csv", header=TRUE) 
 
 # LOOP THROUGH ALL MODELS
-while (i < num_treatments+1) {    # COMMENT IN/OUT TO CHECK FOR ONE TREATMENT, loop through 5 treatments
+#while (i < num_treatments+1) {    # COMMENT IN/OUT TO CHECK FOR ONE TREATMENT, loop through 5 treatments
 
 CO2flux <- CO2flux_0 %>%
   filter(Num == i) %>%    # loop through treatment
@@ -101,11 +97,11 @@ eCO2cost=function(pars){
   return(modCost(model=modelOutput, obs=CO2flux[,1:2]))
 }
 
-# ###one pool model####
+###one pool model####
 eCO2func = function(pars) {
   mod=OnepModel(
     t=days,
-    k = pars[1],
+    k = pars[1], # GUESSES K1
     C0 = Ctotal,
     In = inputs_frame,
     pass=TRUE
@@ -114,7 +110,7 @@ eCO2func = function(pars) {
   return(data.frame(time=days,cummCO2=rowSums(AccR)))
 }
 
-inipars=c(k=.0001)
+inipars=c(k=.0001)  # for Palouse soil control should ~= .0006
 
 eCO2fit=modFit(f=eCO2cost,p=inipars,method="Nelder-Mead",
                upper=c(Inf),lower=c(0))
@@ -127,11 +123,6 @@ fitmod=OnepModel(t=days, k=eCO2fit$par[1],
 fitCumm=getAccumulatedRelease(fitmod)
 a <- rowSums(fitCumm)
 
-#Plot the results, crap fit, which is not unexpected, it also does warn us that Nelder-Mead sucks at single optimization
-# plot(CO2flux[,1:2],type="p",xlab="Days",
-#      ylab="Cummulative respiration (mg C g-1 soil)")  #IT'S NOT mg/g UNLESS I DO DIVIDE BY 50
-# lines(rowSums(fitCumm))
-
 fitCumm1 <- rowSums(fitCumm)
 totalfitCumm[, n] <- fitCumm1
 n <- n + 1
@@ -142,11 +133,12 @@ npars=length(eCO2fit$par)
 AIC_1p=(2*npars)-2*log(eCO2fit$ms)
 AICc_1p=AIC_1p+(((2*npars^2)+2*npars)/(length(CO2flux[,1])-npars-1))
 
-# SO SLOW W THIS ON
+# TEST PLOT
 plot1 <- ggplot() +
-  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) +
-  geom_line(data = fitframe1, aes(x = days, y = fitCumm1)) +
+  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) + # INC data
+  geom_line(data = fitframe1, aes(x = days, y = fitCumm1)) +  # model data
   xlim(0, 500) +
+  #ylim(0, 100) +
   labs(x = 'Time [days]', y = 'Cumulative CO2 Released [mg]', title = '1 Pool Model') +
   theme_C
 plot1
@@ -203,22 +195,24 @@ plot1
 # #   labs(x = 'Time [days]', y = 'Cumulative CO2 Released [mg]', title = '2 Pool Feedback Model') +
 # #   theme_C
 # # plot2pf
+# 
 
+#### FOLLOWING 2 CHUNKS OF CODE (2PS, 2PP) GUESS PARAMETERS ASSUMING K1 AND GAMMA ARE FIXED 
 ###two pool series model####
 eCO2func=function(pars){
   mod=TwopSeriesModel(
     t=CO2flux$time, # run this only for the incubation data, this was "days" and it worked prev. till 1/18 caitlin's comments
     ks=c(k_soil,pars[1]),
     a21=pars[2]*k_soil,
-    C0=Ctotal*c(gamma_soil[i],1-gamma_soil[i]), # pars[4] is gamma, get rid of initila pars4 parameters, calc. howmuch carbon is in the soil control from the model, then add the residue carbon, see how much the total is now, and that is your gamma ((C_initfastsoilcontrolpool+C_residuecarbon)/total C), delete guessing of pars[4] anywhere else
-    In = 0, # run this for 0 since that's the incubation
+    C0=Ctotal*c(gamma_soil[i],1-gamma_soil[i]), # I get this number from my IRMS data. gamma_soil = initial C from soil/(initial C from soil + initial C from residue)
+    In = 0, # run this for 0 since that's just the incubation
     pass=TRUE
   )
   AccR=getAccumulatedRelease(mod)
   return(data.frame(time=CO2flux$time,cummCO2=rowSums(AccR)))
 }
 
-inipars=c(k2=.002, alpha21=0.9)
+inipars=c(k2=.002, alpha21=0.9) # approx. what INC1 values were if I fix k1
 
 
 eCO2fit=modFit(f=eCO2cost,p=inipars,method="Nelder-Mead",
@@ -258,6 +252,7 @@ eCO2func=function(pars){
     ks=c(k_soil, pars[1]), #ONCE YOU HAVE K1
     #ks=c(pars[1],pars[2]),
     gam= gamma_soil[i],
+    #C0=Ctotal*c(gamma_soil[i],1-gamma_soil[i]),
     C0=Ctotal*c(gamma_soil[i],1-gamma_soil[i]),
     In = 0, # was inputs_frame
     pass=TRUE
@@ -266,7 +261,7 @@ eCO2func=function(pars){
   return(data.frame(time=CO2flux$time,cummCO2=rowSums(AccR)))
 }
 
-#inipars=c(k1=1, k2=0.8) #for deeper depths, need different starting values
+#inipars=c(k1=1, k2=0.8, gam = .4) #for deeper depths, need different starting values
 inipars=c(k2=0.002) #ONCE YOU HAVE K1 for deeper depths, need different starting values
 eCO2fit=modFit(f=eCO2cost,p=inipars,method="Nelder-Mead",
                upper=c(Inf),lower=c(0))
@@ -290,15 +285,121 @@ totalfitCumm[, n] <- fitCumm4
 n <- n + 1
 fitframe4 <- data.frame(days, fitCumm4)
 
-## PLOT INC DATA W/ MODEL
+## TEST PLOT
 plot2pp <- ggplot() +
-  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) +
-  geom_line(data = fitframe4, aes(x = days, y = fitCumm4)) +
+  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) + # INC data
+  geom_line(data = fitframe4, aes(x = days, y = fitCumm4)) +  # model data
   xlim(0, 1.5*max(CO2flux$time)) +  # to only see relevant model data
   labs(x = 'Time [days]', y = 'Cumulative CO2 Released [mg]', title = '2 Pool Parallel Model') +
   theme_C
 plot2pp
 
+### FOLLOWING 2 CHUNKS OF CODE (2PS, 2PP)  GUESS ALL PARAMETERS AND DO NOT FIX ANYTHING #########
+###two pool series model####
+eCO2func=function(pars){
+  mod=TwopSeriesModel(
+    t=CO2flux$time, # run this only for the incubation data, this was "days" and it worked prev. till 1/18 caitlin's comments
+    ks=pars[1:2],
+    a21=pars[3]*pars[1],
+    C0=Ctotal*c(pars[4],1-pars[4]), 
+    In = 0, # run this for 0 since that's the incubation
+    pass=TRUE
+  )
+  AccR=getAccumulatedRelease(mod)
+  return(data.frame(time=CO2flux$time,cummCO2=rowSums(AccR)))
+}
+
+inipars=c(k1=0.5,k2=0.05,alpha21=0.5,gamma=0.5)
+
+eCO2fit=modFit(f=eCO2cost,p=inipars,method="Nelder-Mead",
+               upper=c(Inf,Inf,1,1),lower=c(0,0,0,0))
+options(scipen = 999)
+twops_par[[i]] <- eCO2fit$par
+
+#Run the model again with best parameter set
+fitmod=TwopSeriesModel(t=days, ks=eCO2fit$par[1:2], 
+                       a21=eCO2fit$par[3]*eCO2fit$par[1],
+                       C0=Ctotal*c(eCO2fit$par[4],1-eCO2fit$par[4]), 
+                       In = 30,)
+fitCumm=getAccumulatedRelease(fitmod)
+
+# # Try getC to get carbon stocks: ??? why is there a bump in the beginning, is it bc of inputs?
+# Ct1=getC(fitmod)
+# plot(days, Ct1[,2], type="l", ylab="Carbon stocks (mg)", xlab="Time (days)") 
+# lines(days, Ct1[,1], col = 'green')
+
+npars=length(eCO2fit$par)
+AIC_2ps=(2*npars)-2*log(eCO2fit$ms) 
+AICc_2ps=AIC_2ps+(((2*npars^2)+2*npars)/(length(CO2flux[,1])-npars-1)) 
+
+fitCumm3 <- rowSums(fitCumm)
+totalfitCumm[, n] <- fitCumm3
+n <- n + 1
+fitframe3 <- data.frame(days, fitCumm3)
+
+## SO SLOW W THIS ON
+plot2ps <- ggplot() +
+  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) +
+  geom_line(data = fitframe3, aes(x = days, y = fitCumm3)) +
+  xlim(0, 200) +
+  ylim(0, 1000) +
+  labs(x = 'Time [days]', y = 'Cumulative CO2 Released [mg]', title = '2 Pool Series Model') +
+  theme_C
+plot2ps
+
+###two pool parallel model####
+eCO2func=function(pars){
+  mod=TwopParallelModel(
+    t=CO2flux$time, # was days
+    ks=pars[1:2],
+    gam=pars[3],
+    C0=Ctotal*c(pars[3],1-pars[3]), 
+    In = 0, # was inputs_frame
+    pass=TRUE
+  )
+  AccR=getAccumulatedRelease(mod)
+  return(data.frame(time=CO2flux$time,cummCO2=rowSums(AccR)))
+}
+
+inipars=c(k1=0.005,k2=0.000000005,gamma=0.08) #for deeper depths, need different starting values
+
+eCO2fit=modFit(f=eCO2cost,p=inipars,method="Nelder-Mead",
+               upper=c(Inf,Inf,1),lower=c(0,0,0))
+
+twopp_par[[i]] <- eCO2fit$par
+
+#Run the model again with best parameter set
+fitmod=TwopParallelModel(t=days, ks=eCO2fit$par[1:2],
+                         gam=eCO2fit$par[3],
+                         C0=Ctotal*c(eCO2fit$par[3],1-eCO2fit$par[3]),
+                         In = inputs_frame,)
+fitCumm=getAccumulatedRelease(fitmod)
+
+npars=length(eCO2fit$par)
+AIC_2pp=(2*npars)-2*log(eCO2fit$ms) 
+AICc_2pp=AIC_2pp+(((2*npars^2)+2*npars)/(length(CO2flux[,1])-npars-1)) 
+
+#Plot the results
+# plot(CO2flux[,1:2],type="p",xlab="Days",
+#      ylab="Cummulative respiration (mg C g-1 soil)")
+# lines(rowSums(fitCumm))
+
+fitCumm4 <- rowSums(fitCumm)
+totalfitCumm[, n] <- fitCumm4
+n <- n + 1
+fitframe4 <- data.frame(days, fitCumm4)
+
+## SO SLOW W THIS ON
+plot2pp <- ggplot() +
+  geom_point(data = CO2flux, aes(x = time, y = cummCO2), shape = 1) +
+  geom_line(data = fitframe4, aes(x = days, y = fitCumm4)) +
+  xlim(0, 200) +
+  labs(x = 'Time [days]', y = 'Cumulative CO2 Released [mg]', title = '2 Pool Parallel Model') +
+  theme_C
+plot2pp
+### END GUESSING CODE #########
+
+####### SAVE DATA
 ## AIC 
 AICc_1p_tot[i] <- AICc_1p
 # AICc_2pf_tot[i] <- AICc_2pf
@@ -307,6 +408,7 @@ AICc_2pp_tot[i] <- AICc_2pp
 
 #totalfitCumm <- cbind(days, fitCumm1, fitCumm2, fitCumm3, fitCumm4) 
 
+##### LOOPING LOGISTICS
 i <- i+1
 print(i)
 print(n)
